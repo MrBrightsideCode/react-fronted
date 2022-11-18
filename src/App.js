b3c9476c7d08ff9eded585c474931284f9bb1ebd
Victor Benitez <victorbenprivatecontact@gmail.com>
2022-11-18 19:57:47 -0300

    Cambios para ver,crear,editar y eliminar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+//importar nuestros componentes
+import ShowProducts from './components/ShowProducts';
+import CreateProduct from './components/CreateProduct';
+import EditProduct from './components/EditProduct';
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <ShowProducts/> } />
+          <Route path='/create' element={ <CreateProduct/> } />
+          <Route path='/edit/:id' element={ <EditProduct/> } />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
